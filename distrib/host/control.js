66d2ea8ab1d52f54706b23da305b3f871510115f
exzackly <exzackly7@me.com>
2017-10-18 18:08:38 -0400

    Add single step execution to control

--- distrib/host/control.js
+++ distrib/host/control.js
@@ -54,32 +54,34 @@ var TSOS;
             // Note the OS CLOCK.
             var clock = _OSclock;
             // Note the REAL clock in milliseconds since January 1, 1970.
-            var now = new Date().getTime();
+            //var now: number = new Date().getTime();
+            var now = Control.hostGetCurrentDateTime(true);
             // Build the log string.
-            var str = "({ clock:" + clock + ", source:" + source + ", msg:" + msg + ", now:" + now + " })" + "\n";
+            var str = "({ clock:" + clock + ", source:" + source + ", msg:" + msg + ", now:" + now + " })" + "<br>";
             // Update the log console.
             var taLog = document.getElementById("taHostLog");
-            taLog.value = str + taLog.value;
+            taLog.innerHTML = str + taLog.innerHTML;
             // TODO in the future: Optionally update a log database or some streaming service.
         };
-        Control.hostGetCurrentDate = function () {
+        Control.hostGetCurrentDateTime = function (hideYear) {
+            if (hideYear === void 0) { hideYear = false; }
             var currentDate = new Date();
             // JavaScript doesn't have any good date format options; the 0 & slice parts are to ensure uniform padding
             var dateString = currentDate.getFullYear() + "-" +
                 ("0" + (currentDate.getMonth() + 1)).slice(-2) + "-" +
-                ("0" + currentDate.getDate()).slice(-2) + " " +
-                ("0" + currentDate.getHours()).slice(-2) + ":" +
+                ("0" + currentDate.getDate()).slice(-2) + " ";
+            var timeString = ("0" + currentDate.getHours()).slice(-2) + ":" +
                 ("0" + currentDate.getMinutes()).slice(-2) + ":" +
                 ("0" + currentDate.getSeconds()).slice(-2);
-            return dateString;
+            return hideYear === true ? timeString : dateString + timeString;
         };
         Control.hostUpdateDisplayDate = function () {
             // Update date in taskbarDate
-            var currentDateString = Control.hostGetCurrentDate();
+            var currentDateString = Control.hostGetCurrentDateTime();
             document.getElementById("taskbarDate").innerHTML = currentDateString;
         };
         Control.hostSetStatus = function (msg) {
-            document.getElementById("taskbarStatus").innerHTML = "Status : " + msg;
+            document.getElementById("taskbarStatus").innerHTML = "[Status]" + msg;
         };
         Control.hostDisplayBSOD = function () {
             var volcanoImg = document.getElementById("volcano");
@@ -93,11 +95,68 @@ var TSOS;
             // Grab text from taProgramInput
             var program = document.getElementById("taProgramInput").value;
             if (program.length == 0) {
-                return false;
+                return -1;
             } // taProgramInput is empty; nothing to load
-            var re = new RegExp("[^ 0-9a-fA-F]"); // Match any non-hex or space character
+            program = program.replace(/\s+/g, ""); // Remove whitespace
+            var re = new RegExp("[^0-9a-fA-F]"); // Match any non-hex character
             var invalidCharactersFound = re.test(program); // Test program for invalid characters
-            return !invalidCharactersFound; // Return true for valid program; return flipped invalidCharactersFound boolean
+            if (invalidCharactersFound === true) {
+                return -1; // Return value of -1 denotes invalid program
+            }
+            else {
+                return _Scheduler.loadNewProcess(program); // Pass to Scheduler to finish load and assign PID
+            }
+        };
+        Control.hostUpdateDisplayCPU = function () {
+            var CPUElement = document.getElementById("displayCPU");
+            var CPUData = "<table style='table-layout:fixed; width: 100%; text-align: center;'>" +
+                "<tbody><tr><th>PC</th><th>ACC</th><th>X</th><th>Y</th><th>Z</th></tr>" +
+                "<tr><td>" + TSOS.Utils.toHex(_CPU.PC) + "</td><td>" + TSOS.Utils.toHex(_CPU.Acc) + "</td><td>" + TSOS.Utils.toHex(_CPU.Xreg) +
+                "</td><td>" + TSOS.Utils.toHex(_CPU.Yreg) + "</td><td>" + _CPU.Zflag + "</td></tr></tbody>" +
+                "</table>";
+            CPUElement.innerHTML = CPUData;
+        };
+        Control.hostUpdateDisplayMemory = function () {
+            var memoryElement = document.getElementById("displayMemory");
+            //todo: replace 768 with variable
+            var memory = _Memory.getBytes(0, 768);
+            var memoryData = "<table style='table-layout:fixed; width: 100%; text-align: center;'><tbody>";
+            //todo: replace 768 with variable
+            for (var i = 0; i < 768; i += 8) {
+                memoryData += "<tr><td style='font-weight: bold'>0x" + TSOS.Utils.toHex(i, 3) + "</td>" +
+                    "<td align='right'>" + TSOS.Utils.toHex(memory[i]) + "</td><td align='right'>" + TSOS.Utils.toHex(memory[i + 1]) + "</td>" +
+                    "<td align='right'>" + TSOS.Utils.toHex(memory[i + 2]) + "</td><td align='right'>" + TSOS.Utils.toHex(memory[i + 3]) + "</td>" +
+                    "<td align='right'>" + TSOS.Utils.toHex(memory[i + 4]) + "</td><td align='right'>" + TSOS.Utils.toHex(memory[i + 5]) + "</td>" +
+                    "<td align='right'>" + TSOS.Utils.toHex(memory[i + 6]) + "</td><td align='right'>" + TSOS.Utils.toHex(memory[i + 7]) + "</td></tr>";
+            }
+            memoryData += "</tbody></table>";
+            memoryElement.innerHTML = memoryData;
+        };
+        Control.hostUpdateDisplayProcesses = function () {
+            var processData = "<tbody><tr><th>PID</th><th>PC</th><th>ACC</th><th>X</th><th>Y</th>" +
+                "<th>Z</th><th>Priority</th><th>State</th><th>Location</th></tr>";
+            var PIDs = Object.keys(_Scheduler.residentList);
+            if (PIDs.length == 0) {
+                processData += "<tr><td colspan='9'>No programs in execution</td></tr>";
+            }
+            else {
+                for (var i = 0; i < PIDs.length; i++) {
+                    var process = _Scheduler.residentList[PIDs[i]];
+                    var state = "";
+                    if (process.isExecuting === true) {
+                        state = "Executing";
+                    }
+                    else {
+                        state = _CPU.isExecuting === true ? "Waiting" : "Ready";
+                    }
+                    var location = process.segment >= 0 && process.segment < SEGMENT_COUNT ? "Memory" : "Disk";
+                    processData += "<tr><td>" + process.pid + "</td><td>" + TSOS.Utils.toHex(process.PC) + "</td><td>" + TSOS.Utils.toHex(process.Acc) + "</td>" +
+                        ("<td>" + TSOS.Utils.toHex(process.Xreg) + "</td><td>" + TSOS.Utils.toHex(process.Yreg) + "</td><td>" + process.Zflag + "</td>") +
+                        ("<td>" + process.priority + "</td><td>" + state + "</td><td>" + location + "</td></tr>");
+                }
+            }
+            var processesElement = document.getElementById("displayProcessesTable");
+            processesElement.innerHTML = processData;
         };
         //
         // Host Events
@@ -113,6 +172,10 @@ var TSOS;
             // ... Create and initialize the CPU (because it's part of the hardware)  ...
             _CPU = new TSOS.Cpu(); // Note: We could simulate multi-core systems by instantiating more than one instance of the CPU here.
             _CPU.init(); //       There's more to do, like dealing with scheduling and such, but this would be a start. Pretty cool.
+            // ... Create Memory ...
+            _Memory = new TSOS.Memory();
+            // ... Create Scheduler ...
+            _Scheduler = new TSOS.Scheduler();
             // ... then set the host clock pulse ...
             _hardwareClockID = setInterval(TSOS.Devices.hostClockPulse, CPU_CLOCK_INTERVAL);
             // .. and call the OS Kernel Bootstrap routine.
@@ -134,6 +197,18 @@ var TSOS;
             // That boolean parameter is the 'forceget' flag. When it is true it causes the page to always
             // be reloaded from the server. If it is false or not specified the browser may reload the
             // page from its cache, which is not what we want.
+        };
+        Control.hostBtnToggle_Single_Step_Mode = function (btn) {
+            _SSMode = !_SSMode;
+            btn.value = "Single Step: " + (_SSMode === true ? "On" : "Off");
+            if (_Scheduler.residentList[_CPU.pid].isExecuting === true) {
+                _CPU.isExecuting = true;
+            }
+        };
+        Control.hostBtnSingleStepCPU = function (btn) {
+            if (_SSMode === true && _CPU.pid !== -1) {
+                _CPU.isExecuting = true;
+            }
         };
         return Control;
     }());
--- source/host/control.ts
+++ source/host/control.ts
@@ -62,38 +62,38 @@ module TSOS {
             var clock: number = _OSclock;
 
             // Note the REAL clock in milliseconds since January 1, 1970.
-            var now: number = new Date().getTime();
+            //var now: number = new Date().getTime();
+            var now: string = Control.hostGetCurrentDateTime(true);
 
             // Build the log string.
-            var str: string = "({ clock:" + clock + ", source:" + source + ", msg:" + msg + ", now:" + now  + " })"  + "\n";
+            var str: string = "({ clock:" + clock + ", source:" + source + ", msg:" + msg + ", now:" + now  + " })"  + "<br>";
 
             // Update the log console.
             var taLog = <HTMLInputElement> document.getElementById("taHostLog");
-            taLog.value = str + taLog.value;
-
+            taLog.innerHTML = str + taLog.innerHTML;
             // TODO in the future: Optionally update a log database or some streaming service.
         }
 
-        public static hostGetCurrentDate(): string {
+        public static hostGetCurrentDateTime(hideYear: boolean = false): string {
             var currentDate = new Date();
             // JavaScript doesn't have any good date format options; the 0 & slice parts are to ensure uniform padding
             var dateString = currentDate.getFullYear() + "-" +
                 ("0"+(currentDate.getMonth()+1)).slice(-2) + "-" +
-                ("0" + currentDate.getDate()).slice(-2) + " " +
-                ("0" + currentDate.getHours()).slice(-2) + ":" +
+                ("0" + currentDate.getDate()).slice(-2) + " ";
+            var timeString = ("0" + currentDate.getHours()).slice(-2) + ":" +
                 ("0" + currentDate.getMinutes()).slice(-2) + ":" +
                 ("0" + currentDate.getSeconds()).slice(-2);
-            return dateString;
+            return hideYear === true ? timeString : dateString + timeString;
         }
 
         public static hostUpdateDisplayDate(): void {
             // Update date in taskbarDate
-            var currentDateString = Control.hostGetCurrentDate();
-            document.getElementById("taskbarDate").innerHTML=currentDateString;
+            var currentDateString = Control.hostGetCurrentDateTime();
+            document.getElementById("taskbarDate").innerHTML = currentDateString;
         }
 
         public static hostSetStatus(msg: string): void {
-            document.getElementById("taskbarStatus").innerHTML = "Status : " + msg;
+            document.getElementById("taskbarStatus").innerHTML = "[Status]" + msg;
         }
 
         public static hostDisplayBSOD(): void {
@@ -105,13 +105,70 @@ module TSOS {
             (<HTMLImageElement>document.getElementById("errorMessage")).style.display = "inline";
         }
 
-        public static hostLoad(): boolean {
+        public static hostLoad(): number {
             // Grab text from taProgramInput
             var program = (<HTMLInputElement> document.getElementById("taProgramInput")).value;
-            if (program.length == 0) { return false; } // taProgramInput is empty; nothing to load
-            var re = new RegExp("[^ 0-9a-fA-F]"); // Match any non-hex or space character
+            if (program.length == 0) { return -1; } // taProgramInput is empty; nothing to load
+            program = program.replace(/\s+/g, ""); // Remove whitespace
+            var re = new RegExp("[^0-9a-fA-F]"); // Match any non-hex character
             var invalidCharactersFound = re.test(program); // Test program for invalid characters
-            return !invalidCharactersFound; // Return true for valid program; return flipped invalidCharactersFound boolean
+            if (invalidCharactersFound === true) {
+                return -1; // Return value of -1 denotes invalid program
+            } else {
+                return _Scheduler.loadNewProcess(program); // Pass to Scheduler to finish load and assign PID
+            }
+        }
+
+        public static hostUpdateDisplayCPU(): void {
+            var CPUElement = <HTMLInputElement> document.getElementById("displayCPU");
+            var CPUData = "<table style='table-layout:fixed; width: 100%; text-align: center;'>" +
+                "<tbody><tr><th>PC</th><th>ACC</th><th>X</th><th>Y</th><th>Z</th></tr>" +
+                "<tr><td>" + Utils.toHex(_CPU.PC) + "</td><td>" + Utils.toHex(_CPU.Acc) + "</td><td>" + Utils.toHex(_CPU.Xreg) +
+                "</td><td>" + Utils.toHex(_CPU.Yreg) + "</td><td>" + _CPU.Zflag + "</td></tr></tbody>" +
+                "</table>";
+            CPUElement.innerHTML = CPUData;
+        }
+
+        public static hostUpdateDisplayMemory(): void {
+            var memoryElement = <HTMLInputElement> document.getElementById("displayMemory");
+            //todo: replace 768 with variable
+            var memory = _Memory.getBytes(0, 768);
+            var memoryData = "<table style='table-layout:fixed; width: 100%; text-align: center;'><tbody>";
+            //todo: replace 768 with variable
+            for (var i = 0; i < 768; i += 8) {
+                memoryData += "<tr><td style='font-weight: bold'>0x" + Utils.toHex(i, 3) + "</td>" +
+                    "<td align='right'>" + Utils.toHex(memory[i]) + "</td><td align='right'>" + Utils.toHex(memory[i+1]) + "</td>" +
+                    "<td align='right'>" + Utils.toHex(memory[i+2]) + "</td><td align='right'>" + Utils.toHex(memory[i+3]) + "</td>" +
+                    "<td align='right'>" + Utils.toHex(memory[i+4]) + "</td><td align='right'>" + Utils.toHex(memory[i+5]) + "</td>" +
+                    "<td align='right'>" + Utils.toHex(memory[i+6]) + "</td><td align='right'>" + Utils.toHex(memory[i+7]) + "</td></tr>";
+            }
+            memoryData += "</tbody></table>";
+            memoryElement.innerHTML = memoryData;
+        }
+
+        public static hostUpdateDisplayProcesses(): void {
+            var processData = "<tbody><tr><th>PID</th><th>PC</th><th>ACC</th><th>X</th><th>Y</th>" +
+                "<th>Z</th><th>Priority</th><th>State</th><th>Location</th></tr>";
+            var PIDs = Object.keys(_Scheduler.residentList);
+            if (PIDs.length == 0) {
+                processData += "<tr><td colspan='9'>No programs in execution</td></tr>";
+            } else {
+                for (var i = 0; i < PIDs.length; i++) {
+                    var process = _Scheduler.residentList[PIDs[i]];
+                    var state = "";
+                    if (process.isExecuting === true) {
+                        state = "Executing";
+                    } else {
+                        state = _CPU.isExecuting === true ? "Waiting" : "Ready";
+                    }
+                    var location = process.segment >= 0 && process.segment < SEGMENT_COUNT ? "Memory" : "Disk";
+                    processData += `<tr><td>${process.pid}</td><td>${Utils.toHex(process.PC)}</td><td>${Utils.toHex(process.Acc)}</td>` +
+                        `<td>${Utils.toHex(process.Xreg)}</td><td>${Utils.toHex(process.Yreg)}</td><td>${process.Zflag}</td>` +
+                        `<td>${process.priority}</td><td>${state}</td><td>${location}</td></tr>`;
+                }
+            }
+            var processesElement = <HTMLTableElement> document.getElementById("displayProcessesTable");
+            processesElement.innerHTML = processData;
         }
 
         //
@@ -131,6 +188,12 @@ module TSOS {
             // ... Create and initialize the CPU (because it's part of the hardware)  ...
             _CPU = new Cpu();  // Note: We could simulate multi-core systems by instantiating more than one instance of the CPU here.
             _CPU.init();       //       There's more to do, like dealing with scheduling and such, but this would be a start. Pretty cool.
+
+            // ... Create Memory ...
+            _Memory = new Memory();
+
+            // ... Create Scheduler ...
+            _Scheduler = new Scheduler();
 
             // ... then set the host clock pulse ...
             _hardwareClockID = setInterval(Devices.hostClockPulse, CPU_CLOCK_INTERVAL);
@@ -156,5 +219,20 @@ module TSOS {
             // be reloaded from the server. If it is false or not specified the browser may reload the
             // page from its cache, which is not what we want.
         }
+
+        public static hostBtnToggle_Single_Step_Mode(btn): void {
+            _SSMode = !_SSMode;
+            btn.value = "Single Step: " + (_SSMode === true ? "On" : "Off");
+            if (_Scheduler.residentList[_CPU.pid].isExecuting === true) {
+                _CPU.isExecuting = true;
+            }
+        }
+
+        public static hostBtnSingleStepCPU(btn): void {
+            if (_SSMode === true && _CPU.pid !== -1) { // Single Step Mode on and CPU has process loaded
+                _CPU.isExecuting = true;
+            }
+        }
+
     }
 }
